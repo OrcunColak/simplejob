900ea25f0e58e616c818be0a36bb16187a563b6c
Orçun Çolak <orcun.colak@hazelcast.com>
2023-08-21 14:41:51 +0300

    new job examples

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,14 +6,10 @@ import com.hazelcast.jet.pipeline.Pipeline;
 import com.hazelcast.jet.pipeline.Sinks;
 import com.hazelcast.jet.pipeline.test.TestSources;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-
 public class Main {
 
     // Parallel job
-    public static void main(String[] args) throws Exception {
+    /*public static void main(String[] args) throws Exception {
         HazelcastInstance hz = Hazelcast.bootstrappedInstance();
         ExecutorService executorService = Executors.newFixedThreadPool(1);
 
@@ -33,7 +29,7 @@ public class Main {
                 .writeTo(Sinks.logger());
         hz.getJet().newJob(pipeline);
     }
-
+    */
     // Simple job
     /* Simple
     public static void main(String[] args) {
@@ -50,4 +46,27 @@ public class Main {
 
         hz.getJet().newJob(pipeline);
     }*/
+
+    // Exception job
+    /*
+    public static void main(String[] args) {
+        throw new RuntimeException("Everything's OK");
+
+    }*/
+
+    // Exception job
+    public static void main(String[] args) {
+        Pipeline pipeline = Pipeline.create();
+        pipeline.readFrom(TestSources.itemStream(10))
+                .withoutTimestamps()
+                .map(t -> t)
+                .filter(event -> event.sequence() % 2 == 0)
+                .setName("filter out odd numbers")
+                .writeTo(Sinks.logger());
+
+        HazelcastInstance hz = Hazelcast.bootstrappedInstance();
+
+        hz.getJet().newJob(pipeline).join();
+
+    }
 }
